0783e6ee8203d065af1aa9fd7b0078585ce84705
nary <nary12321@gmail.com>
2021-09-25 23:28:55 +0800

    click "Eyedrone" text on NavBar to go to home page.

--- src/components/NavigatorBar.jsx
+++ src/components/NavigatorBar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function NavigatorBar(props) {
     return <div style={{width: "100%"}}>
         <header style={{width: "100%"}}>
-            <h1>Eyedrone</h1>
+            <h1 style={{cursor: "pointer"}} onClick={()=>{window.location.href = "/";}}>Eyedrone</h1>
             {props.linkTo !== "null" ?
                     <button className="show-data" onClick={()=>{window.location.href = "/";}}>
                         主畫面
