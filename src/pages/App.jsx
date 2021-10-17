200f23318cad9155ba7dee8ee9aa54adaa363977
wancat <wancat@wancat.cc>
2021-10-17 17:42:58 +0800

    ModelDetail Page

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import NewModule from "./NewModule";
 import NewPredict from "./NewPredict";
 import Home from "./Home";
+import ModelDetail from "./ModelDetail";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import NavigatorBar from "../components/NavigatorBar";
@@ -17,6 +18,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path="/predicts/new" component={NewPredict} />
           <Route path="/models/new" component={NewModule} />
+          <Route path="/models/:id-:name" component={ModelDetail} />
         </Switch>
       </BrowserRouter>
     </QueryClientProvider>
