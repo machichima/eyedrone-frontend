b3bbe9f5635318f33903118997c02d965379ce2e
nary <nary12321@gmail.com>
2021-11-03 18:42:27 +0800

    在 model detail 中加上 prediction 表格

--- src/pages/ModelDetail.jsx
+++ src/pages/ModelDetail.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import axios from "../components/axios";
+import Table from "react-bootstrap/Table";
 
 async function getModelDetail(id) {
   const res = await axios.get(`/api/models/${id}/`);
@@ -29,10 +30,52 @@ function ModelDetail() {
       <p>R2：{r2}</p>
     </div>
   ));
+
+  const predictionTable = (
+    <div>
+      <Table
+          striped
+          bordered
+          hover
+          style={{ tableLayout: "fixed", width: "70%" }}
+      >
+        <thead>
+          <tr>
+            <th>座標\資料</th>
+            <th>do</th>
+            <th>bod</th>
+            <th>ss</th>
+            <th>nh3n</th>
+            <th>average</th>
+            {/* {data.predictions.map(({x, y}) => (<th>{x}, {y}</th>))} */}
+          </tr>
+        </thead>
+        <tbody>
+          {data.predictions.map(({"do":do_val, bod, ss, nh3n, x, y}, index) => {
+            let err_do = (data.points[index].do - do_val)/data.points[index].do;
+            let err_bod = (data.points[index].bod - bod)/data.points[index].bod;
+            let err_ss = (data.points[index].ss - ss)/data.points[index].ss;
+            let err_nh3n = (data.points[index].nh3n - nh3n)/data.points[index].nh3n;
+
+            return(
+                <tr>
+                  <td>{x}, {y}</td>
+                  <td>{data.points[index].do}/{do_val}</td>
+                  <td>{data.points[index].bod}/{bod}</td>
+                  <td>{data.points[index].ss}/{ss}</td>
+                  <td>{data.points[index].nh3n}/{nh3n}</td>
+                  <td>{(err_do+err_bod+err_ss+err_nh3n)/4}</td>
+                </tr>
+          )})}
+          </tbody>
+      </Table>
+    </div>
+  );
   return (
     <div>
       <h1>{data.name}</h1>
       <div>{substanceList}</div>
+      <div>{predictionTable}</div>
     </div>
   );
 }
